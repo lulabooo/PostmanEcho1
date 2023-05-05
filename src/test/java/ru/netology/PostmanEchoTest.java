3d1d0535de9b44b547ef24ce89fd9273bff09278
Maria Loginova <marialog@yandex.ru>
2023-05-05 09:43:28 +0300

    added test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostRawTextTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет, мир!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет, мир!"));
+    }
+}
