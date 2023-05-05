52af5bddac265ed3691d991dea35e827fe36ac96
Maria Loginova <marialog@yandex.ru>
2023-05-05 09:52:54 +0300

    again ok

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -10,7 +10,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Привет, мирpp!")
+                .body("Привет, мир!")
                 .when()
                 .post("/post")
                 .then()
